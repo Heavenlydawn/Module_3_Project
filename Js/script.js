d4dfd4640515b4a6bdc76b5aa69a93ea210f1271
Heaven Gabriel <gabrielheavenlydawn@gmail.com>
2023-08-22 15:36:25 +0100

    Final JS Modification

--- Js/script.js
+++ Js/script.js
@@ -13,6 +13,41 @@ function showLinks() {
   } else {
     navLinks.classList.add("show__links");
   }
-  //   navLinks.classList.toggle("show__links");
+  // navLinks.classList.toggle("show__links");
 }
 
+// Get All The Buttons
+const nftBtnFilter = document.querySelectorAll(".btn__filter");
+
+// Loop through each button and add a click event listener
+nftBtnFilter.forEach((button) => {
+  button.addEventListener("click", () => {
+    // Remove the "active" class from all buttons
+    nftBtnFilter.forEach((btn) => {
+      btn.classList.remove("btn__active");
+    });
+
+    // Add the "active" class to the clicked button
+    button.classList.add("btn__active");
+  });
+});
+
+// Filter
+const btn__filter = document.querySelectorAll(".btn__filter");
+const discover__card = document.querySelectorAll(".discover__card");
+
+btn__filter.forEach((b) => {
+  b.addEventListener("click", () => {
+    const nftFilter = b.getAttribute("data-filter");
+    discover__card.forEach((card) => {
+      if (
+        nftFilter === "All Categories" ||
+        card.getAttribute("data-filter") === nftFilter
+      ) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
+});
